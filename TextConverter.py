ab54b1fb375a2efc6591becd6a546c79278ded79
vanessa-sbq <up202207919@up.pt>
2023-12-27 21:18:34 +0000

    Started implementing 3D text

--- TextConverter.py
+++ TextConverter.py
@@ -1,38 +1,54 @@
-from PyQt6.QtWidgets import QDialog, QGraphicsView, QGraphicsScene, QVBoxLayout, QGraphicsPixmapItem, QSizePolicy
+from PyQt6.QtWidgets import QDialog, QGraphicsView, QGraphicsScene, QVBoxLayout, QGraphicsPixmapItem, QSizePolicy, QWidget, QPushButton, QHBoxLayout, QSplitter
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtCore import Qt
-#from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont
 
 class TextConverterMenu(QDialog):
     def __init__(self, image_path):
         super().__init__()
+        
+        self.image_path = image_path;
 
         self.window_width, self.window_height = 800, 500
         self.setMinimumSize(self.window_width, self.window_height)
         self.setWindowTitle("Text Converter")
  
-        layout = QVBoxLayout(self)
+        main_layout = QHBoxLayout(self)
+        
+        # Create a splitter to divide the window into two parts
+        splitter = QSplitter(Qt.Orientation.Horizontal)
 
         # Create QGraphicsView and QGraphicsScene
         self.graphicsView = QGraphicsView(self)
         self.graphicsScene = QGraphicsScene(self)
         self.graphicsView.setScene(self.graphicsScene)
-        layout.addWidget(self.graphicsView)
+        self.graphicsView.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.graphicsView.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+        self.graphicsView.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+        self.graphicsView.wheelEvent = self.zoom
+        main_layout.addWidget(self.graphicsView)
         
+        # Add QGraphicsView to the left side of the splitter
+        splitter.addWidget(self.graphicsView)
+                
+        # Create a user menu on the right side
+        user_menu_layout = QVBoxLayout()
+        user_menu_widget = QWidget(self)
+        user_menu_widget.setLayout(user_menu_layout)
+        button1 = QPushButton("Add 3D text")
+        button1.pressed.connect(self.add3dText)
+        button2 = QPushButton("Button 2")
+        user_menu_layout.addWidget(button1)
+        user_menu_layout.addWidget(button2)
+        splitter.addWidget(user_menu_widget)
+        splitter.setSizes([int(self.window_width * 0.75), int(self.window_width * 0.25)])
+
+        main_layout.addWidget(splitter)
+
         # Load the image
         pixmap = QPixmap(image_path)
         item = QGraphicsPixmapItem(pixmap)
         self.graphicsScene.addItem(item)
-        
-        # Set the view size policy to allow resizing
-        self.graphicsView.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-
-        # Enable scroll bars for zooming
-        self.graphicsView.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
-        self.graphicsView.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
-
-        # Connect the zoom functionality to the wheel event
-        self.graphicsView.wheelEvent = self.zoom
         
         self.show()
         
@@ -43,28 +59,39 @@ class TextConverterMenu(QDialog):
 
         self.graphicsView.setTransform(self.graphicsView.transform().scale(factor, factor))
         
-    #def add3dText():
-        # with Image.open("chell.jpg") as im:
-        #     px = im.load()
+    def add3dText(self):
+        with Image.open(self.image_path) as im:
+            px = im.load()
             
-        #     fontsize = int(input("fontsize: "))
-        #     title = input("title: ")
+            fontsize = int(input("fontsize: "))
+            title = input("title: ")
             
-        #     draw = ImageDraw.Draw(im)
-        #     font = ImageFont.truetype("arial.ttf", fontsize)  # Specify your font file and size
-        #     depth = int(input("depth: "))
+            draw = ImageDraw.Draw(im)
+            font = ImageFont.truetype("arial.ttf", fontsize)  # Specify your font file and size
+            depth = int(input("depth: "))
 
-        #     text_position = (0, 0)  # Position of the text
-        #     a = 0
-        #     for i in range(depth):
-        #         text_color = (i, i, i)  # RGB color for the text
-        #         text_position = (0 + i, 10 + i)  # Position of the text
-        #         draw.text(text_position, title, font=font, fill=text_color)
-        #         a = i
-        #     text_color = (0, 255, 255)
-        #     draw.text(text_position, title, font=font, fill=text_color)
-            
-        #     im.save("output.png")
+            text_position = (0, 0)  # Position of the text
+            a = 0
+            for i in range(depth):
+                text_color = (i, i, i)  # RGB color for the text
+                text_position = (0 + i, 10 + i)  # Position of the text
+                draw.text(text_position, title, font=font, fill=text_color)
+                a = i
+            text_color = (0, 255, 255)
+            draw.text(text_position, title, font=font, fill=text_color)
+            im.save("output.png")
+            print("IMAGE SAVED")
+            self.updatePreview()
+        
+    def updatePreview(self):
+        # Update QGraphicsScene with the modified image
+        pixmap = QPixmap("output.png")
+        item = QGraphicsPixmapItem(pixmap)
+        self.graphicsScene.clear()  # Clear existing items in the scene
+        self.graphicsScene.addItem(item)
+
+        # Set the new scene to the graphicsView
+        self.graphicsView.setScene(self.graphicsScene)
         
         
         
