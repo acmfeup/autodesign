96d4c28da64bcd41b6bd0db000e0a26ec1eebfd3
vanessa-sbq <up202207919@up.pt>
2023-12-27 20:17:48 +0000

    Small change in code

--- TextConverter.py
+++ TextConverter.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import QLabel, QDialog
 from PyQt6.QtGui import QPixmap
+#from PIL import Image, ImageDraw, ImageFont
 
 class TextConverterMenu(QDialog):
     def __init__(self, image_path):
@@ -14,6 +15,29 @@ class TextConverterMenu(QDialog):
         label.setPixmap(pixmap)
  
         self.show()
+        
+    #def add3dText():
+        # with Image.open("chell.jpg") as im:
+        #     px = im.load()
+            
+        #     fontsize = int(input("fontsize: "))
+        #     title = input("title: ")
+            
+        #     draw = ImageDraw.Draw(im)
+        #     font = ImageFont.truetype("arial.ttf", fontsize)  # Specify your font file and size
+        #     depth = int(input("depth: "))
+
+        #     text_position = (0, 0)  # Position of the text
+        #     a = 0
+        #     for i in range(depth):
+        #         text_color = (i, i, i)  # RGB color for the text
+        #         text_position = (0 + i, 10 + i)  # Position of the text
+        #         draw.text(text_position, title, font=font, fill=text_color)
+        #         a = i
+        #     text_color = (0, 255, 255)
+        #     draw.text(text_position, title, font=font, fill=text_color)
+            
+        #     im.save("output.png")
         
         
         
